ca74c5810206c72c625a556dfa949d85f4851571
exponentialbit1024 <shah255@purdue.edu>
2017-08-24 19:15:28 -0400

    Commit

--- CS252/lab1/MyMalloc.c
+++ CS252/lab1/MyMalloc.c
@@ -122,7 +122,7 @@ static void * allocateObject(size_t size)
   pthread_mutex_unlock(&mutex);
 
   size_t roundedSize = (size + sizeof(FreeObject) + 7) & ~7;
-  FreeListNode *p = _freeList -> free_list_node._next;
+  FreeObject *p = _freeList -> free_list_node._next;
   while(p != _freeList){
     if (getSize(p->boundary_tag) >= roundedSize && getSize(p->boundary_tag) < roundedSize + sizeof(FreeObject) + 8) {
       printf("Got it");
